ade4dfc2f37ca9a176e4576b12e09a2e74f9bdc0
Adhy <adhywiranatap@gmail.com>
2017-07-02 18:07:10 +0700

    update App

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>REACT IS 4W350M3</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          This app do nothing. Let's develop this app using TDD!
         </p>
       </div>
     );
