3fa921ad6a4988598e90d43eb1a4914f4fa52b72
Adhy <adhywiranatap@gmail.com>
2017-07-02 21:26:52 +0700

    fix todo reducer initial state

--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -9,9 +9,10 @@ export const deleteTodo = (todos, todoId) => {
   return todos.filter(todo => todo.id !== todoId);
 };
 
-export default (state, action) => {
+export default (state = initialState, action) => {
   switch (action.type) {
     case 'ADD_TODO': return addTodo(state, action.payload);
     case 'DELETE_TODO': return deleteTodo(state, action.payload);
+    default: return state;
   }
 };
