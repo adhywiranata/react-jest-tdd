5bd725dec2dd6090a272b83c1c821b9154c61490
Adhy <adhywiranatap@gmail.com>
2017-07-02 21:49:57 +0700

    update test for ConnectedHelloSection

--- src/components/HelloSection.test.js
+++ src/components/HelloSection.test.js
@@ -1,16 +1,14 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { shallow, mount } from 'enzyme';
 
 import { HelloSection } from './HelloSection';
+import ConnectedHelloSection from './HelloSection';
+import store from '../store/configureStore';
 
 describe('<HelloSection> component ', () => {
-
-  /*
-  ============
-  OUTPUT TESTS
-  ============
-  */
   const helloSectionWrapper = shallow(<HelloSection />);
+
   it('renders correctly', () => {
     expect(helloSectionWrapper).toHaveLength(1);
   });
@@ -34,5 +32,15 @@ describe('<HelloSection> component ', () => {
     const helloSectionUl = helloSectionWrapper.find('ul');
     const LiInsidehelloSectionUl = helloSectionUl.find('li');
     expect(LiInsidehelloSectionUl).toHaveLength(0);
+  });
+});
+
+describe('<ConnectedHelloSection> redux-connected component  ', () => {
+  it('renders correctly', () => {
+    const ConnectedHelloSectionWrapper = shallow(
+      <Provider store={store}>
+        <ConnectedHelloSection />
+      </Provider>);
+    expect(ConnectedHelloSectionWrapper).toHaveLength(1);
   });
 });
