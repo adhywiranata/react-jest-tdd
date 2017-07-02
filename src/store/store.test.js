f335eafd3d8ecc2974565d38dc2abe0812a2f93c
Adhy <adhywiranatap@gmail.com>
2017-07-02 21:34:02 +0700

    add more store unit test cases

--- src/store/store.test.js
+++ src/store/store.test.js
@@ -7,4 +7,28 @@ describe('Store ', () => {
 
     expect(actualTodos).toEqual(expectedTodos);
   });
+
+  it('should add todos when ADD_TODO action dispatched', () => {
+    const newTodo = {
+      id: 1,
+      task: 'TDD!',
+      completed: false,
+    };
+    const expectedTodosState = [newTodo];
+
+    store.dispatch({ type: 'ADD_TODO', payload: newTodo });
+    const actualTodosState = store.getState().todos;
+
+    expect(actualTodosState).toEqual(expectedTodosState);
+  });
+
+  it('should delete a todo when DELETE_TODO action dispatched', () => {
+    const todoToDeleteId = 1;
+    const expectedTodosState = [];
+    
+    store.dispatch({ type: 'DELETE_TODO', payload: todoToDeleteId });
+    const actualTodosState = store.getState().todos;
+
+    expect(actualTodosState).toEqual(expectedTodosState);
+  });
 })
